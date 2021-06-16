52833082817c6fe17bba981582030f0284d8a780
oh-yeah-sea-kit2 <kyohei.t.204@gmail.com>
2021-06-17 05:50:54 +0900

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,125 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+type FeeClassification = {
+  name: string;
+  description: string;
+  unitPrice: number;
+  numOfPeople: number;
+  totalPrice: number;
+}
+
+type DetailProps = {
+  classification: FeeClassification;
+}
+
+class Detail extends React.Component<DetailProps, {}> {
+  render() {
+    return (
+      <div >
+        <div className="classification-name">{this.props.classification.name}</div>
+        <div className="description">{this.props.classification.description}</div>
+        <div className="unit-price">{this.props.classification.unitPrice}</div>
+        <div className="num-people">
+          <select value={this.props.classification.numOfPeople}>
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+          </select>
+          <span>名</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Summary extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="party">
+          <input type="text" className="party" value="0" />
+          <span>名様</span>
+        </div>
+        <div className="total-amount">
+          <span>合計</span>
+          <input type="text" className="total-amount" value="0" />
+          <span>円</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+class AdmissionFeeCalculator extends React.Component {
+  private details: DetailProps[] = [
+    {
+      classification: {
+        name: "大人",
+        description: "",
+        unitPrice: 1000,
+        numOfPeople: 0,
+        totalPrice: 0
+      }
+    },
+    {
+      classification: {
+        name: "学生",
+        description: "中学生・高校生",
+        unitPrice: 700,
+        numOfPeople: 0,
+        totalPrice: 0,
+      }
+    },
+    {
+      classification: {
+        name: "子ども",
+        description: "小学生",
+        unitPrice: 300,
+        numOfPeople: 0,
+        totalPrice: 0,
+      }
+    },
+    {
+      classification: {
+        name: "幼児",
+        description: "未就学",
+        unitPrice: 0,
+        numOfPeople: 0,
+        totalPrice: 0,
+      }
+    },
+  ];
+
+  render() {
+    const detailsJsx = this.details.map((fc, idx) => {
+      return (
+        <Detail key={idx.toString()}classification={fc.classification} />
+      );
+    });
+
+    return (
+      <>
+        {detailsJsx}
+        <Summary />
+      </>
+    );
+  }
+}
+
+const App: React.FC = () => {
+  return (
+    <div className="main">
+      <AdmissionFeeCalculator />
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +128,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
